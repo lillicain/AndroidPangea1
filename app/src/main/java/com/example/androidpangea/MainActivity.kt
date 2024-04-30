e7085f05fc17ca48fae6c205a8c19ec977ea28a3
lilli <lilliancrabbit@gmail.com>
2024-04-30 12:53:22 -0600

    Goes to main screen

--- app/src/main/java/com/example/androidpangea/MainActivity.kt
+++ app/src/main/java/com/example/androidpangea/MainActivity.kt
@@ -11,34 +11,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidpangea.ui.theme.AndroidPangeaTheme
+import com.example.androidpangea.views.mainScreen.MainScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidPangeaTheme { // A surface container using the 'background' color from the theme
+            AndroidPangeaTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                 MainScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!", modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidPangeaTheme {
-        Greeting("Android")
+
     }
 }
--- app/src/main/java/com/example/androidpangea/views/mainScreen/MainScreen.kt
+++ app/src/main/java/com/example/androidpangea/views/mainScreen/MainScreen.kt
@@ -1,8 +1,9 @@
 package com.example.androidpangea.views.mainScreen
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun MainScreen() {
-
+Text(text = "Main")
 }
