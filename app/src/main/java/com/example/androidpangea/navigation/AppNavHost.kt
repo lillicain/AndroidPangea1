5ed53d9f982700361ccf0a549b533104d8b8093b
lilli <lilliancrabbit@gmail.com>
2024-05-08 13:00:49 -0600

    Clickable Text Works

--- app/src/main/java/com/example/androidpangea/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/androidpangea/navigation/AppNavHost.kt
@@ -50,7 +50,7 @@ fun AppNavHost(
         startDestination = if (isSplashScreenFinished) {
             NavigationItem.Main.route
         } else {
-            NavigationItem.Auth.route
+            NavigationItem.SignIn.route
         }
     ) {
 
