df78d17a79a9fe3762fc05fcff9410051b0f3c05
lilli <lilliancrabbit@gmail.com>
2024-05-15 06:38:38 -0600

    Runs

--- app/src/main/java/com/example/androidpangea/views/cameraScreen/PhotoScreen.kt
+++ app/src/main/java/com/example/androidpangea/views/cameraScreen/PhotoScreen.kt
@@ -1,8 +1,14 @@
 package com.example.androidpangea.views.cameraScreen
 
 import android.annotation.SuppressLint
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -12,9 +18,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.example.androidpangea.navigation.NavigationItem
 import com.example.androidpangea.navigation.bar.BottomNavigationBar
 import com.example.androidpangea.views.subviews.ImagePicker
 import com.ramcosta.composedestinations.annotation.Destination
@@ -26,20 +39,18 @@ import com.ramcosta.composedestinations.annotation.Destination
 fun PhotoScreen(
     navController: NavController
 ) {
-
-
     Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = { BottomNavigationBar(navController = navController) }) {
-        ImagePicker()
 
+        ImagePicker()
 
         Column(modifier = Modifier.fillMaxSize()) {
             TopAppBar(
                 title = {
-                    Text("Sign Out", fontSize = 16.sp)
+                    Text("Back to Camera", fontSize = 18.sp)
                 },
                 navigationIcon = {
                     IconButton(onClick = {
-                        navController.popBackStack()
+                        navController.navigate(NavigationItem.Camera.route)
                     }) {
                         Icon(
                             imageVector = Icons.Default.ArrowBack, contentDescription = "Back",
