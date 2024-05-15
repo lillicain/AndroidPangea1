6d8d4b279b454a471c69dcafa3310ac5620641d0
lilli <lilliancrabbit@gmail.com>
2024-05-15 16:39:08 -0600

    Works

--- app/src/main/java/com/example/androidpangea/views/authentication/SignInScreen.kt
+++ app/src/main/java/com/example/androidpangea/views/authentication/SignInScreen.kt
@@ -1,5 +1,6 @@
 package com.example.androidpangea.views.authentication
 
+import android.annotation.SuppressLint
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -14,6 +15,7 @@ import com.example.androidpangea.views.authentication.components.SignInContent
 import com.example.androidpangea.views.authentication.components.SignInTopBar
 
 
+@SuppressLint("SuspiciousIndentation")
 @Composable
 @ExperimentalComposeUiApi
 fun SignInScreen(
